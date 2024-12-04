0a8fc2c3f6de4cbfc7518e8f4c4480c207358e94
madebusuyi <madebusuyi@demonware.net>
2024-12-04 14:01:22 +0000

    Formattd bash file

--- gen-folder.sh
+++ gen-folder.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
 if [[ -z $1 ]]; then
-  echo "no input"
-  exit 1
+    echo "no input"
+    exit 1
 fi
 
 DAY_NUMBER=$1
 
 if [ -d "./day_${DAY_NUMBER}" ]; then
-  echo "folder already exists"
-  exit 1
+    echo "folder already exists"
+    exit 1
 fi
 
 function make_solution_folder() {
-  folder_name="solution_$1"
-  mkdir "$folder_name"
-  cd "$folder_name" || exit 1
-  cat "../../solution-template.go" >> main.go
-  go mod init main.go
-  cd ..
+    folder_name="solution_$1"
+    mkdir "$folder_name"
+    cd "$folder_name" || exit 1
+    cat "../../solution-template.go" >> main.go
+    go mod init main.go
+    cd ..
 }
 
 mkdir "day_${DAY_NUMBER}"
@@ -28,4 +28,3 @@ curl --cookie "$(cat ../.env)" -o "input.txt" "https://adventofcode.com/2024/day
 
 make_solution_folder "0"
 make_solution_folder "1"
-
