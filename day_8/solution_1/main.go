7c7c2320309d6daf93b6cd20ca7c9e601f821e42
FrontRowWithJ <adebusum@tcd.ie>
2024-12-08 09:18:56 +0000

    Shortened solutions

--- day_8/solution_1/main.go
+++ day_8/solution_1/main.go
@@ -6,20 +6,15 @@ import (
 	"strings"
 )
 
-type Point struct {
-	X int
-	Y int
-}
-
 func main() {
 	input, _ := os.ReadFile("../input.txt")
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
-	antennas := map[string][]Point{}
-	grid := map[Point]bool{}
+	antennas := map[string][][2]int{}
+	grid := map[[2]int]bool{}
 	for i, line := range lines {
 		for j, cell := range strings.Split(line, "") {
 			if cell != "." {
-				antennas[cell] = append(antennas[cell], Point{i, j})
+				antennas[cell] = append(antennas[cell], [2]int{i, j})
 			}
 		}
 	}
@@ -27,13 +22,13 @@ func main() {
 		for i := 0; i < len(points); i++ {
 			for j := i + 1; j < len(points); j++ {
 				a, b := points[i], points[j]
-				x, y := a.X-b.X, a.Y-b.Y
+				x, y := a[0]-b[0], a[1]-b[1]
 				grid[points[i]], grid[points[j]] = true, true
-				for x1, y1 := a.X+x, a.Y+y; x1 >= 0 && y1 >= 0 && x1 < len(lines) && y1 < len(lines); x1, y1 = x1+x, y1+y {
-					grid[Point{x1, y1}] = true
+				for x1, y1 := a[0]+x, a[1]+y; x1 >= 0 && y1 >= 0 && x1 < len(lines) && y1 < len(lines); x1, y1 = x1+x, y1+y {
+					grid[[2]int{x1, y1}] = true
 				}
-				for x2, y2 := b.X-x, b.Y-y; x2 >= 0 && y2 >= 0 && x2 < len(lines) && y2 < len(lines); x2, y2 = x2-x, y2-y {
-					grid[Point{x2, y2}] = true
+				for x2, y2 := b[0]-x, b[1]-y; x2 >= 0 && y2 >= 0 && x2 < len(lines) && y2 < len(lines); x2, y2 = x2-x, y2-y {
+					grid[[2]int{x2, y2}] = true
 				}
 			}
 		}
