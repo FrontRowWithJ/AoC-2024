79fb3b736708f28c063d338a4116207fc12c7268
FrontRowWithJ <adebusum@tcd.ie>
2024-12-13 09:35:10 +0000

    Shortened solution

--- day_13/solution_1/main.go
+++ day_13/solution_1/main.go
@@ -8,13 +8,11 @@ import (
 
 func main() {
 	input, _ := os.ReadFile("../input.txt")
-	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 	totalTokens := 0
-	for i := 0; i < len(lines); i += 4 {
+	for _, s := range strings.Split(strings.TrimSpace(string(input)), "\n\n") {
 		var aX, aY, bX, bY, prizeX, prizeY int
-		fmt.Sscanf(lines[i+0], "Button A: X+%d, Y+%d", &aX, &aY)
-		fmt.Sscanf(lines[i+1], "Button B: X+%d, Y+%d", &bX, &bY)
-		fmt.Sscanf(lines[i+2], "Prize: X=%d, Y=%d", &prizeX, &prizeY)
+		fmt.Sscanf(s, "Button A: X+%d, Y+%d\nButton B: X+%d, Y+%d\nPrize: X=%d, Y=%d",
+			&aX, &aY, &bX, &bY, &prizeX, &prizeY)
 		prizeX, prizeY = prizeX+10000000000000, prizeY+10000000000000
 		D, Dx, Dy := aX*bY-bX*aY, prizeX*bY-bX*prizeY, aX*prizeY-prizeX*aY
 		if D != 0 && Dx == (Dx/D)*D && Dy == (Dy/D)*D {
